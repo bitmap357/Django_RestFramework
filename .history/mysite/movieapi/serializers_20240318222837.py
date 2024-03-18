9ef6556b6714a90c5eded3454f7596ca071a8d57
bitmap357 <edakoyeram@gmail.com>
2024-03-18 22:29:22 +0000

    Adding image to serialzer

--- .history/mysite/movieapi/serializers_20240318222837.py
+++ .history/mysite/movieapi/serializers_20240318222837.py
@@ -4,7 +4,6 @@ from rest_framework import serializers
 
 class MovieSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(max_length=None, use_url=True)
-    
     class Meta:
         model = Movie
         fields = ('name', 'description', 'rating', 'image')
