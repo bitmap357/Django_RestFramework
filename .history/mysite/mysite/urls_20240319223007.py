a6f3d67414422eba78179404fd83884f002364ba
bitmap357 <edakoyeram@gmail.com>
2024-03-19 22:31:09 +0000

    Adding image url

--- .history/mysite/mysite/urls_20240319223007.py
+++ .history/mysite/mysite/urls_20240319223007.py
@@ -17,9 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('movieapi/', include('movieapi.urls')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROO)
